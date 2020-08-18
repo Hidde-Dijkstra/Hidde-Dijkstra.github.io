c740be9c92cc6a63f8397d3a5568ecdc58bf67cb
Hidde-Dijkstra <hidde.dijkstra@gmail.com>
2020-08-18 12:25:56 +0200

    move pc

--- project-book/_build/jupyter_execute/master-thesis/monolayer.py
+++ project-book/_build/jupyter_execute/master-thesis/monolayer.py
@@ -2,15 +2,29 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import drawSvg as draw
 from myst_nb import glue
 
 def rot_mat(θ):
     return np.array([[np.cos(θ), -np.sin(θ)], [np.sin(θ), np.cos(θ)]])
 
-We base our tight binding model of monolayer WSe$_2$ (mWSe$_2$) on the three band model (TBM) by Liu et al. {cite}'three_band'. In the TBM of mWSe2 the hopping is modeled using only the tungsten sites, forming a triangular lattice in the $xy$ plane:
+We base our tight binding model of monolayer WSe$_2$ (mWSe$_2$) on the three band model (TBM) by Liu et al. {cite}`three_band`. In the TBM of mWSe2 the hopping is modeled using only the tungsten sites, forming a triangular lattice in the $xy$ plane with real space unit vectors:
+```{math}
+\vec a_1=\begin{pmatrix}1\\ 0\end{pmatrix}\text{ and } \vec a_2=\frac12\begin{pmatrix}1\\ \sqrt{3}\end{pmatrix},    
+```
+and reciprocal lattice vectors:
+```{math}
+\vec b_1=2\pi\begin{pmatrix}1\\ -1/\sqrt{3}\end{pmatrix}\text{ and } \vec b_2=2\pi\begin{pmatrix}0\\ 2/\sqrt{3}\end{pmatrix},    
+```
+where length is in units of the lattice spacing $a$ between two tungsten atoms.
+
+We consider one atom as the origin and label every other atom using the integers $i$ and $j$ which connect this atom to their origin with the vector $i
+\vec a_1 + j\vec a_2$:
 
 class LatVec:
+    # We identify each atom as the two integers i and j which connect it to the origin. 
+    # Using a pythonic object we can define how two of these vectors interact.
     a_1 = np.array([1, 0])
     a_2 = np.array([1/2, np.sqrt(3)/2])
     b_1 = 2*np.pi * np.array([1, -1/np.sqrt(3)])
@@ -30,6 +44,7 @@ class LatVec:
         return (self.i==other.i) & (self.j==other.j)
     
     def __and__(self, other):
+        # A simple way to get all hopping vectors from two unit vectors.
         return [self, other.neg(), self.neg()+other.neg(), self.neg(), other, self+other]
     
     def __mul__(self, other):
@@ -50,27 +65,114 @@ class LatVec:
         else:
             return self.scale*(self.i*self.a_1 + self.j*self.a_2)
     
-    def plot(self, container, atom_radius=0.2, atom_color='darkblue', θ=0, atom="", bonds=False, **kwargs):
-        origin = self.vec 
+    def plot(self, container, atom_radius=0.2, atom_color='darkblue', θ=0, atom="", bonds=False, displace=0, **kwargs):
+        origin = self.vec + displace
         a_list = [self.a_1, self.a_2, self.a_1-self.a_2]      
         if θ != 0:
             origin = rot_mat(θ) @ self.vec
             a_list = [rot_mat(θ) @ a for a in a_list]
-        for a in a_list:
-            container.append(draw.Line(*(origin-a/2), *(origin+a/2), **kwargs))
-            gradient = draw.RadialGradient(*origin, atom_radius)
+        if bonds: 
+            for a in a_list:
+                container.append(draw.Line(*(origin-a/2), *(origin+a/2), **kwargs))
+        gradient = draw.RadialGradient(*origin, atom_radius)
         gradient.addStop(0, 'white', 1)
         gradient.addStop(1, atom_color, 1)
         container.append(draw.Circle(*origin, atom_radius, fill=gradient, **kwargs))
         container.append(draw.Text(atom, atom_radius, *origin, text_anchor='middle', alignment_baseline="central"))
         return container
     
-container = draw.Drawing(5, 3, origin='center', displayInline=False)
-for (i, j) in [(0, 0), (1, 0), (0, 1), (1, -1), (-1, 0), (0, -1), (-1, 1), 
-               (-2, 1), (-2, 0), (-1, -1), (2, 0), (1, 1), (2, -1)]:
-    container = LatVec(i, j).plot(container, stroke= 'black', bonds=True, atom='W', stroke_width=0.02)
-#glue('fig', container.setRenderSize(900), display=False)
-container.setRenderSize(900)
+container = draw.Drawing(4, 2.3, origin='center', displayInline=False)
+for (i, j) in [(i, j) for i in range(-3, 3) for j in range(-2, 2)]:
+    container = LatVec(i, j).plot(container, stroke='black', atom_color='red', bonds=True, atom=str(i)+','+str(j), stroke_width=0.015)
+    container = LatVec(i, j).plot(container, stroke='black', atom_color='blue', atom="Se2", stroke_width=0.01, 
+                                  displace=[1/2, 1/3], atom_radius=0.15)
+container.setRenderSize(1000)
+glue('fig:lattice', container, display=False)
+
+```{glue:figure} fig:lattice
+```
+
+The symmetric point group of this triangular lattice is $D_3$ with elements $\{E, C_3, C^2_3,\sigma_v, \sigma_v', \sigma_v''\}$. The rotations are in the $xy$ plane, while the reflections are with respect to the $yz$, $xz$ and $xy$ planes. $C_6$ is not a symmetry element of this lattice since this operation exchanges triangles containing Se$_2$ and empty triangles.
+
+We restrict the orbitals of tungsten to the $d$ orbitals $d_{x^2-y^2}$, $d_{xy}$ and $d_{z^2}$, which dictate the low energy $K$ points. The other orbitals are either at a different energy scale or do not mix with these orbitals due to the symmetries of the lattice. Since $C_6$ is not a symmetry element, $C_2=C_6\prod C_3$ is not either.
+
+class Orbital:  
+    
+    def lobe(self, color, rotate=0, translate=(0, 0), stroke="black", **kwargs):
+        gradient = draw.RadialGradient(0, 100, 50)
+        gradient.addStop(0, 'white', 0.7)
+        gradient.addStop(173.21, color, 0.7)
+        transform = "translate(" + " ".join([str(i) for i in translate]) + ")\nrotate(" + str(rotate) + " 0 0)"
+        return draw.Path(d="M 0,0 C -173.21,-200 173.21,-200 0,0 z", stroke=stroke, fill=gradient, transform=transform, **kwargs)
+    
+    def circle(self, color, ellipse=False, rotate=0, translate=(0, 0), stroke="black", **kwargs):
+        gradient = draw.RadialGradient(0, 0, 50)
+        gradient.addStop(0, 'white', 0.7)
+        gradient.addStop(173.21, color, 0.7)
+        transform = "rotate(" + str(rotate) + " 0 0)\ntranslate(" + " ".join([str(i) for i in translate]) + ")"
+        if ellipse:
+            clip = draw.ClipPath()
+            clip.append(draw.Ellipse(0, 0, 50, 12.5, transform=transform))
+            return draw.Ellipse(0, 0, 100, 25, stroke=stroke, fill=gradient, transform=transform, **kwargs) 
+        else:
+            return draw.Circle(0, 0, 50, stroke=stroke, fill=gradient, transform=transform, **kwargs)
+
+class d_xy(Orbital):
+    
+    def __init__(self, container, translate=(0, 0), rotate=0):
+        container.append(self.lobe("dodgerblue", rotate=85+rotate, translate=translate))
+        container.append(self.lobe("red", rotate=95+rotate, translate=translate))
+        container.append(self.lobe("red", rotate=275+rotate, translate=translate))
+        container.append(self.lobe("dodgerblue", rotate=265+rotate, translate=translate))
+        
+class d_z2(Orbital):
+    
+    def __init__(self, container, translate=(0, 0), rotate=0):
+        container.append(self.lobe("dodgerblue", rotate=180+rotate, translate=translate))
+        container.append(self.circle("red", ellipse=True, rotate=rotate, translate=translate))
+        container.append(self.lobe("dodgerblue", rotate=rotate, translate=translate))
+
+container = draw.Drawing(1000, 350, origin="center")
+d_z2(container)
+d_xy(container, translate=(300, 0))
+d_xy(container, translate=(-300, 0))
+glue('fig:d_z2-d_xy', container, display=False)
+
+```{glue:figure} fig:d_z2-d_xy
+```
+
+var_dic = {
+    "t_1": 0.034, 
+    "t_2": 0.263, 
+    "t_3": -0.207, 
+    "t_12": 0.329, 
+    "t_13": 0.486, 
+    "t_23": 0.457, 
+    "ε_1": 2.179, 
+    "ε_3": 0.942, 
+    "λ_SOC": 0.228
+}
+t_1, t_2, t_3, t_12, t_13, t_23, ε_1, ε_3, λ_SOC = var_dic.values()
+for var_key in var_dic:
+    glue("var:"+var_key, var_dic[var_key], display=False)
+
+```{admonition} GGA coupling constants in eV
+Coupling constants for WSe$_2$, subscripts 1, 2 and 3 refer to the $d_{x^2-y^2}$, $d_{xy}$ and $d_{z^2}$ orbitals respectively. These constants are for NN hopping left to right with respect to the $x$-axis. $\varepsilon_i$ are the on-site energies where $\varepsilon_1=\varepsilon_2$ due to symmetry. $\lambda_\text{SOC}$ is the spin orbit coupling. These are the GGA constants from {cite}`three_band`.
+| $t_1$| $t_2$| $t_3$| $t_{12}$| $t_{13}$| $t_{23}$| $\varepsilon_1$| $\varepsilon_3$| $\lambda_{SOC}$|
+|:----:|:----:|:----:|:-------:|:-------:|:-------:|:--------------:|:--------------:|:--------------:|
+|{glue:}`var:t_1`|{glue:}`var:t_2`|{glue:}`var:t_3`|{glue:}`var:t_12`|{glue:}`var:t_13`|{glue:}`var:t_23`|{glue:}`var:ε_1`|{glue:}`var:ε_3`|{glue:}`var:λ_SOC`|
+where all energies are measured in eV. 
+```
+
+
+ and collect them in the matrix $\mathcal E$:
+```{math}
+    \mathcal E = \begin{pmatrix}
+        t_1&t_{12}&t_{13}\\
+        -t_{12}&t_2&t_{23}\\
+        t_{13}&-t_{23}&t_3
+    \end{pmatrix}.
+```
 
 class Supercell:
     hop_list = LatVec(1, 0) & LatVec(0, -1)
@@ -153,26 +255,15 @@ $$
 f(x) = x^2
 $$
 
-import svgwrite
 
-dwg = svgwrite.Drawing()
-link = dwg.add(dwg.a("http://link.to/internet"))
-square = link.add(dwg.rect((0, 0), (1, 1), fill='blue'))
 
-display(dwg.get_xml())
+```{bibliography} references.bib
+```
 
-container = draw.Drawing(6, 3, origin='center')
-container.append(draw.Line(-3, 0, 3, 0, stroke='white', stroke_width='0.7'))
-gradient = draw.RadialGradient(0, 0, 1)
-gradient.addStop(0, 'white', 1)
-gradient.addStop(1, 'darkblue', 1)
-container.append(draw.Circle(0, 0, 1, fill=gradient, stroke_width=0.1, stroke='white'))
-container.append(draw.Text("W", 1, 0, 0, text_anchor='middle', alignment_baseline="central"))
-container.setRenderSize(200)
-container
+[0, 0] + np.array([2, 0])
 
-container.height = 200
-container
+" ".join(["i", "r"])
 
-container
+```{bibliography} references.bib
+```
 
--- project-book/master-thesis/intro.md
+++ project-book/master-thesis/intro.md
@@ -1,3 +1,4 @@
-# Twisted bilayer WSe_2
+# Twisted bilayer WSe$_2$
 
-In October last year Wang et al published an experimental paper on the magic continuum in twisted bilayer tungsten-diselinide (tWSe$_2$) {cite}'tWSe2_experiment'. Here they found flat bands similar to the ones found in twisted bilayer graphene but at a larger twist angle of approximately 5 degrees. At low temperatures varying the electron density gives rise to insulating and superconducting states. Due to the large twist angle we aim to build a tight binding (TB) Hamiltonian for tWSe$_2$ without having to resort to a continuum model {cite}'continuum_model'. 
+In October last year Wang et al published an experimental paper on the magic continuum in twisted bilayer tungsten-diselinide (tWSe$_2$) {cite}`tWSe2_experiment`. Here they found flat bands similar to the ones found in twisted bilayer graphene but at a larger twist angle of approximately 5 degrees. At low temperatures varying the electron density gives rise to insulating and superconducting states. Due to the large twist angle we aim to build a tight binding (TB) Hamiltonian for tWSe$_2$ without having to resort to a continuum model {cite}`continuum_model`. 
+
