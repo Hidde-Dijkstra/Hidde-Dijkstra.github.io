9069d1aa6349e79088ccbbb4264fbe77ca7a10c3
Hidde-Dijkstra <hidde.dijkstra@gmail.com>
2020-09-14 16:12:56 +0200

    updating files

---
+++ project-book/_build/jupyter_execute/topological-insulators/week_1_band_structure.py
@@ -0,0 +1,8 @@
+# Bloch Theorem and Tight-Binding Model
+
+We study the behavior of electrons in periodic lattices of atoms. Every atom is identical and as such we have translational invariance for any $\mathbf T=\sum n_i\mathbf a_i$, where the $a_i$ are the lattice vectors of the lattice. We model the lattice with a periodic potential $V(\mathbf r)$ that obeys the translational symmetry: $V(\mathbf r)=V(\mathbf r+\mathbf T)$. Now the stationary Schrödinger equation describes the behavior of an electron in this potential:
+```{math}
+H=-\frac{1}{2m}\nabla_{\mathbf r}^2+V(\mathbf r),
+```
+here, and in the future, we take $\hbar =1$.
+
